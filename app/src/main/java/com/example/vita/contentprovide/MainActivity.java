1367e8a4ac30f6cbee831cb39a633c31a63e706c
vita888 <13793899@qq.com>
2017-06-28 13:08:38 +0800

    读取联系人

---
+++ app/src/main/java/com/example/vita/contentprovide/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.vita.contentprovide;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+private  final  String TAG ="#######";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri,null,null,null, null);
+        while (cursor.moveToNext()){
+            String cName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String cNum = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            Log.i(TAG, "name: "+cName+";number:"+cNum);
+            Log.i(TAG,"=================");
+        }
+        cursor.close();
+    }
+}
