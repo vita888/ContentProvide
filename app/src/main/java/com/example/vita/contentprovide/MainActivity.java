913477a9de473b25b42f3db874cb26fb06ecf989
vita888 <13793899@qq.com>
2017-06-28 15:19:01 +0800

    查找联系人信息

--- app/src/main/java/com/example/vita/contentprovide/MainActivity.java
+++ app/src/main/java/com/example/vita/contentprovide/MainActivity.java
@@ -7,9 +7,20 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-private  final  String TAG ="#######";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private  final  String TAG ="#######";
+    private Button mButton;
+    private EditText mEditText;
+    private ContentResolver mContentResolver;
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +30,9 @@ private  final  String TAG ="#######";
     @Override
     protected void onStart() {
         super.onStart();
-        ContentResolver contentResolver = getContentResolver();
+        mContentResolver = getContentResolver();
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        Cursor cursor = contentResolver.query(uri,null,null,null, null);
+        Cursor cursor = mContentResolver.query(uri,null,null,null, null);
         while (cursor.moveToNext()){
             String cName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String cNum = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
@@ -30,5 +41,38 @@ private  final  String TAG ="#######";
             Log.i(TAG,"=================");
         }
         cursor.close();
+
+
+        mButton = (Button)findViewById(R.id.searchbtn);
+        mEditText = (EditText)findViewById(R.id.searchet);
+        mTextView = (TextView)findViewById(R.id.result);
+
+        mButton.setOnClickListener(this);
+
+    }
+
+    private  void  queryContact(String name){
+        Uri uriNum = Uri.parse("content://com.android.contacts/data/phones/filter/" + name);
+
+        Cursor cursor = mContentResolver.query(uriNum,new String[]{"display_number"},null
+        ,null,null);
+
+        String number;
+
+        if (cursor.moveToFirst()){
+            number = cursor.getString(0);
+            Log.i(TAG, " "+name+"的电话号码是"+number);
+            mTextView.append(" "+name+"的电话号码是"+number);
+        }
+        cursor.close();
+    
+    }
+    @Override
+    public void onClick(View v) {
+       switch(v.getId()) {
+           case R.id.searchbtn:
+               String name = mEditText.getText().toString();
+               queryContact(name);
+       }
     }
 }
